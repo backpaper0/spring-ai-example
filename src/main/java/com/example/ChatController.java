ec893572c40131afcf32e3402a1c1a896b82092c
Uragami Taichi <backpaper0@gmail.com>
2024-02-29 09:02:40 +0900

    ストリーミングを試す

--- src/main/java/com/example/ChatController.java
+++ src/main/java/com/example/ChatController.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.StreamingChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,17 +10,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Flux;
+
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
 
     @Autowired
     private ChatClient chatClient;
+    @Autowired
+    private StreamingChatClient streamingChatClient;
 
     @PostMapping
     public Object post(@RequestParam String text) {
         Prompt prompt = new Prompt(text);
         ChatResponse resp = chatClient.call(prompt);
         return resp.getResult();
+    }
+
+    @PostMapping("/stream")
+    public Flux<String> stream(@RequestParam String text) {
+        Prompt prompt = new Prompt(text);
+        return streamingChatClient.stream(prompt)
+                .map(resp -> resp.getResult().getOutput().getContent());
     }
 }
