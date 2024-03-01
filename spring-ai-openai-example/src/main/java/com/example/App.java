804e854c14ab31bddfa0ed8939c96c868c50fab0
Uragami Taichi <backpaper0@gmail.com>
2024-03-02 07:12:43 +0900

    メインクラスをリネーム

--- spring-ai-openai-example/src/main/java/com/example/App.java
+++ spring-ai-openai-example/src/main/java/com/example/App.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAiExampleApplication {
+public class App {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAiExampleApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
