c2a6021b0a3f70731cad3ec55345dd108cd93970
Uragami Taichi <backpaper0@gmail.com>
2024-12-01 09:22:20 +0900

    Spring AI 1.0.0-M4へアップグレード

--- spring-ai-openai-example/src/main/java/com/example/ChatController.java
+++ spring-ai-openai-example/src/main/java/com/example/ChatController.java
@@ -2,9 +2,6 @@ package com.example;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.ChatOptions;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,25 +24,21 @@ public class ChatController {
 
     @PostMapping
     public Object post(@RequestParam String text) {
-        Prompt prompt = new Prompt(text);
-        ChatResponse resp = chatClient.prompt(prompt).call().chatResponse();
+        ChatResponse resp = chatClient.prompt(text).call().chatResponse();
         return resp.getResult();
     }
 
     @PostMapping(path = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> stream(@RequestParam String text) {
-        Prompt prompt = new Prompt(text);
-        return chatClient.prompt(prompt).stream().chatResponse()
+        return chatClient.prompt(text).stream().chatResponse()
                 .map(resp -> resp.getResult().getOutput().getContent());
     }
 
     @PostMapping("/fn")
     public Object postWithFunctionCalling(@RequestParam String text) {
-        ChatOptions chatOptions = OpenAiChatOptions.builder()
-                .withFunction("weatherFunction")
-                .build();
-        Prompt prompt = new Prompt(text, chatOptions);
-        ChatResponse resp = chatClient.prompt(prompt).call().chatResponse();
+        ChatResponse resp = chatClient.prompt(text)
+                .functions("weatherFunction")
+                .call().chatResponse();
         return resp.getResult().getOutput();
     }
 }
